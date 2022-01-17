744a38dfd7ded6385359c530070acd2174862c12
Brendon8 <brendon.majzik@georgebrown.ca>
2022-01-17 14:37:25 -0500

    Initial Commit

---
+++ Hello/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Hello
+//
+//  Created by Brendon Majzik on 2022-01-17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
